ce9f056c08023222819532e3fab72f5df17b9ff2
Divyansh Katare <93079133+KDivyansh@users.noreply.github.com>
2023-12-25 20:12:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h2").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
+// Responsive Hamburger Menu
+const navbar = document.querySelector(".navbar");
+const navLinks = document.querySelector(".nav-links");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+        navbar.classList.remove("active");
+    }
+});
