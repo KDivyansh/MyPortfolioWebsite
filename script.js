4db86c96e04efd15a2df95588719563dad6a8d64
Divyansh Katare <93079133+KDivyansh@users.noreply.github.com>
2023-12-27 15:31:17 +0530

    script.js
    Added landing page animation

--- script.js
+++ script.js
@@ -2,30 +2,42 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let interval = null;
 
-document.querySelector("h2").onmouseover = event => {  
+// Function to run the text animation
+const runTextAnimation = (targetElement) => {
   let iteration = 0;
-  
+
   clearInterval(interval);
-  
+
   interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+    targetElement.innerText = targetElement.dataset.value
       .split("")
       .map((letter, index) => {
         if(index < iteration) {
-          return event.target.dataset.value[index];
+          return targetElement.dataset.value[index];
         }
-      
-        return letters[Math.floor(Math.random() * 26)]
+
+        return letters[Math.floor(Math.random() * 26)];
       })
       .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
+
+    if(iteration >= targetElement.dataset.value.length){ 
       clearInterval(interval);
     }
-    
+
     iteration += 1 / 3;
   }, 30);
-}
+};
+
+// Run the text animation when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+  const h2Element = document.querySelector("h2");
+  runTextAnimation(h2Element);
+});
+
+// Event listener for the hover effect
+document.querySelector("h2").onmouseover = event => {
+  runTextAnimation(event.target);
+};
 
 // Responsive Hamburger Menu
 const navbar = document.querySelector(".navbar");
@@ -33,11 +45,11 @@ const navLinks = document.querySelector(".nav-links");
 const hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener("click", () => {
-    navbar.classList.toggle("active");
+  navbar.classList.toggle("active");
 });
 
 window.addEventListener("resize", () => {
-    if (window.innerWidth > 768) {
-        navbar.classList.remove("active");
-    }
+  if (window.innerWidth > 768) {
+    navbar.classList.remove("active");
+  }
 });
